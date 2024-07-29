3cdef36a813451390ae8b86db1b543f766d3ac6d
joshuandeleva <joshuandeleva90@gmail.com>
2024-07-30 00:14:44 +0300

    expo env , camera and expo location on mapbox

---
+++ components/Map.tsx
@@ -0,0 +1,10 @@
+import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN ?? null);
+export default function Map() {
+  return (
+    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
+      <Camera followZoomLevel={16} followUserLocation />
+      <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+    </MapView>
+  );
+}

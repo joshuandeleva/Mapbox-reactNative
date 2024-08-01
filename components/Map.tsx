ee5fe9375b22b0e4bb13d0d377042da02a9e1de7
joshuandeleva <joshuandeleva90@gmail.com>
2024-08-01 22:05:30 +0300

    markers

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,29 @@
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, {
+  Camera,
+  Images,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+
+import pin from '~/assets/pin.png';
+import scooters from '~/data/scooters.json';
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_TOKEN ?? null);
 export default function Map() {
+  const points = scooters.map((scooter) => point([scooter.long, scooter.lat]));
   return (
     <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
-      <Camera followZoomLevel={16} followUserLocation />
+      <Camera followZoomLevel={10} followUserLocation />
       <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+      <ShapeSource id="scooters" shape={featureCollection(points)}>
+        <SymbolLayer
+          id="scooters"
+          style={{ iconImage: 'pin', iconSize: 0.5, iconAllowOverlap: true }}
+        />
+        <Images images={{ pin }} />
+      </ShapeSource>
     </MapView>
   );
 }
